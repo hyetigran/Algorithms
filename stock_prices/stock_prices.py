9783ef0fe967fdc466154a5a024a786e69ca5518
hyetigran <tigran.asriyan@gmail.com>
2020-02-20 17:29:48 -0500

    stock prices task complete and passing tests

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,28 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    max_price = -100000
+    current_min = 0
+
+    if len(prices) < 2:
+        return "Not enough information provided"
+
+    for i in range(len(prices)):
+        top_price = max(prices[i+1:], default=0)
+        if top_price - prices[i] > max_price:
+            max_price = top_price - prices[i]
+    return max_price
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
